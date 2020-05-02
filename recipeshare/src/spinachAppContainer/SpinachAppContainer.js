4e6540a77d1d665bf6526c6eef075f840e2520ef
pothers <markpothers@hotmail.com>
2020-05-02 12:23:53 -0500

    spinach app container code tidy, and sql query updates to remove duplicates

--- recipeshare/src/spinachAppContainer/SpinachAppContainer.js
+++ recipeshare/src/spinachAppContainer/SpinachAppContainer.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import { FlatList, ScrollView, SafeAreaView, Image, KeyboardAvoidingView, View, ActivityIndicator } from 'react-native'
-// import { styles } from './newRecipeStyleSheet'
+import { ScrollView, SafeAreaView, Image, KeyboardAvoidingView, View, ActivityIndicator } from 'react-native'
 import { centralStyles } from '../centralStyleSheet'
-import FilterMenu from '../functionalComponents/filterMenu'
 
 
 export default class SpinachAppContainer extends React.Component {
@@ -15,59 +13,34 @@ export default class SpinachAppContainer extends React.Component {
     }
 
     render() {
-      // console.log('instructions')
-      // console.log(this.state.instructions)
-      // console.log('order')
-      // console.log(this.state.instructionsOrder)
-    //   const instructionsHeight = (this.state.instructionsOrder.length+1)*9.5
-      // console.log(instructionsHeight)
-        if (this.props.scrollingEnabled)
-        {
+        if (this.props.scrollingEnabled){
             return (
                 <SafeAreaView style={centralStyles.fullPageSafeAreaView}>
-                <Image
-                source={require('../dataComponents/spinach.jpg')}
-                style={centralStyles.spinachFullBackground}
-                // resizeMode={"cover"}
-                />
-                {/* <KeyboardAvoidingView style={centralStyles.fullPageKeyboardAvoidingView}> */}
-                {this.props.awaitingServer && <View style={centralStyles.activityIndicatorContainer}><ActivityIndicator style={centralStyles.activityIndicator } size="large" color="#104e01" /></View>}
-                {/* {this.state.filterDisplayed ? <FilterMenu handleCategoriesButton={this.handleCategoriesButton} newRecipe={true} confirmButtonText={"Save"} title={"Select categories for your recipe"}/> : null} */}
-                {/* {this.state.choosingPicture ? this.renderPictureChooser() : null} */}
-                <ScrollView style={centralStyles.fullPageScrollView}
-                nestedScrollEnabled={true}
-                scrollEnabled={this.state.scrollingEnabled}
-                keyboardShouldPersistTaps={'always'}
-                // onPanResponderGrant={() => console.log('granted')}
-                // onPanResponderRelease={() => console.log('released')}
-                // disableScrollViewPanResponder={true}
-                ref={(c) => { this.scrollView = c; }}
-                >
-                    {this.props.children}
-
-
-                </ScrollView>
-                {/* </KeyboardAvoidingView> */}
+                    <Image
+                        source={require('../dataComponents/spinach.jpg')}
+                        style={centralStyles.spinachFullBackground}
+                    />
+                    {this.props.awaitingServer && <View style={centralStyles.activityIndicatorContainer}><ActivityIndicator style={centralStyles.activityIndicator } size="large" color="#104e01" /></View>}
+                    <ScrollView style={centralStyles.fullPageScrollView}
+                        nestedScrollEnabled={true}
+                        scrollEnabled={this.state.scrollingEnabled}
+                        keyboardShouldPersistTaps={'always'}
+                        ref={(c) => { this.scrollView = c; }}
+                    >
+                        {this.props.children}
+                    </ScrollView>
                 </SafeAreaView>
             )
         }
         else {
             return (
                 <SafeAreaView style={centralStyles.fullPageSafeAreaView}>
-                <Image
-                source={require('../dataComponents/spinach.jpg')}
-                style={centralStyles.spinachFullBackground}
-                // resizeMode={"cover"}
-                />
-                {/* <KeyboardAvoidingView style={centralStyles.fullPageKeyboardAvoidingView}> */}
-                {this.props.awaitingServer && <View style={centralStyles.activityIndicatorContainer}><ActivityIndicator style={centralStyles.activityIndicator } size="large" color="#104e01" /></View>}
-                {/* {this.state.filterDisplayed ? <FilterMenu handleCategoriesButton={this.handleCategoriesButton} newRecipe={true} confirmButtonText={"Save"} title={"Select categories for your recipe"}/> : null} */}
-                {/* {this.state.choosingPicture ? this.renderPictureChooser() : null} */}
-
+                    <Image
+                        source={require('../dataComponents/spinach.jpg')}
+                        style={centralStyles.spinachFullBackground}
+                    />
+                    {this.props.awaitingServer && <View style={centralStyles.activityIndicatorContainer}><ActivityIndicator style={centralStyles.activityIndicator } size="large" color="#104e01" /></View>}
                     {this.props.children}
-
-
-                {/* </KeyboardAvoidingView> */}
                 </SafeAreaView>
             )
         }
