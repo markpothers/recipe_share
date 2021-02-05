2ead121a69bfb6dcf5f81a2a36714510eea8dec0
Mark Pothecary <markpothers@hotmail.com>
2021-02-05 11:57:07 -0600

    rspec setup

---
+++ recipeshare_api/spec/controllers/recipe_likes_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe RecipeLikesController, type: :controller do
+  describe "GET index" do
+    # it "returns a 200" do
+    #   request.headers["Authorization"] = "foo"
+    #   get :show
+    #   expect(response).to have_http_status(:ok)
+    # end
+    it "thinks 1 + 1 = 2" do
+      expect(1 + 1).to equal(2)
+    end
+  end
+end
---
+++ recipeshare_api/spec/models/recipe_make_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe RecipeMake, type: :model do
+  it "thinks 1 + 1 = 2" do
+    expect(1 + 1).to equal(2)
+  end
+end
