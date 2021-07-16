03082eda408227a29b8aa51efa1bd8c1a45efc3f
Mark Pothecary <markpothers@hotmail.com>
2021-07-15 21:59:02 -0500

    bug fix

--- recipeshare_api/app/controllers/recipes_controller.rb
+++ recipeshare_api/app/controllers/recipes_controller.rb
@@ -15,7 +15,7 @@ class RecipesController < ApplicationController
         @filters = Recipe.get_filters(params["listType"], @chef.id, params["queryChefID"], params["filters"], params["serves"], params["cuisine"], params["search_term"])
         # puts "rendering"
         # byebug
-        render jsoncd: {recipes: @recipes, cuisines: @cuisines, serves: @serves, filters: @filters}
+        render json: {recipes: @recipes, cuisines: @cuisines, serves: @serves, filters: @filters}
     end
 
     # def new
