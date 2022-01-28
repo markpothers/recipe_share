4c39ed153f3838a99e777fef8fcba565e81dee2c
Mark Pothecary <markpothers@hotmail.com>
2022-01-27 20:37:35 -0600

    expo upgrade (#36)

--- recipeshare/enzymeSetup.js
+++ recipeshare/enzymeSetup.js
@@ -1,6 +1,6 @@
 import 'react-native';
 import 'jest-enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import Enzyme from 'enzyme';
 
 /**
