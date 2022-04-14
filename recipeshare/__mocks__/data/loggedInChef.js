6572b60df30c1ad9566cf4e70d66e635009e7c26
Mark Pothecary <markpothers@hotmail.com>
2022-04-14 17:24:29 -0500

    url update

--- recipeshare/__mocks__/data/loggedInChef.js
+++ recipeshare/__mocks__/data/loggedInChef.js
@@ -7,7 +7,7 @@ const loggedInChef = {
 	"first_name": null,
 	"hex": "myFakeHex",
 	"id": 1,
-	"image_url": "https://storage.googleapis.com/chef-avatars-ac1cff8d11c908f3cf8613121b61683221152250/139409814d7b033cc8bce85bebfda9ccfceaa84b.jpg?GoogleAccessId=recipe-share-image-handler%40recipe-share-272202.iam.gserviceaccount.com&Expires=1651363200&Signature=fY5v1N63jdUmF4x%2F78Y6%2FxxAm1Y9%2FoSs68j6r5O%2FhvlpO441fi7vOxGhlPjpbQMfa7qYjwa8ynjXgzR%2BVSFpt2VRw7GekwBfsKaraYXVn7SYoKvOK2mLf8%2FvyzONp0YCdEZV2KBCkwe2QNt6jNY4QnrWd84j6dFDJJqE4M91Km5%2F8dURebqHSc58M1Luw4QGLegWnLUtwR1xUymAyhn8A0IF%2BGwZX1oyj2cznmQbooxPnysX6o6zp8TXMXhdA4%2FUyUyjox9FCUDI7qm%2FSVYxjwjAmx%2B1iqXysE5ltIFxnBvpuKQwKG1wBt9JRS7J2f4mct%2FMowdls3rHju8Km9XkuA%3D%3D",
+	"image_url": "https://robohash.org/sedsuntsit.png?size=300x300&set=set1",
 	"is_admin": true,
 	"is_member": false,
 	"last_name": null,
