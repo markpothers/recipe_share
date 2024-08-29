aa8d115e94252a285bfd0f3d72149e878dbc7c42
Mark Pothecary <markpothers@hotmail.com>
2024-08-29 17:48:53 -0500

    package updates and test fixes

--- recipeshare/jestSetup.js
+++ recipeshare/jestSetup.js
@@ -8,18 +8,18 @@ jest.mock("@react-native-voice/voice");
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
 
 //copy this line in some way to many invalid component name errors
-jest.mock("react-native-vector-icons/MaterialCommunityIcons", () => "Icon")
-jest.mock("react-native-vector-icons/FontAwesome", () => "Icon")
+jest.mock("react-native-vector-icons/MaterialCommunityIcons", () => "Icon");
+jest.mock("react-native-vector-icons/FontAwesome", () => "Icon");
 
 // imports stock AsyncStorage + any modifications I've made in the file
 // also allows you to specify some return value in there
 // import mockAsyncStorage from './__mocks__/@react-native-async-storage/async-storage';
 // jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 jest.mock("expo-secure-store", () => ({
-	getItemAsync: jest.fn()
-}))
+	getItemAsync: jest.fn(),
+}));
 
-jest.mock("react-native-webview", () => ({}))
+jest.mock("react-native-webview", () => ({}));
 
 // fixes a bug where this is undefined.  It was removed from react.reanimated at some point
 // and I think some dependencies have not been updated to account for it, at least, not in
